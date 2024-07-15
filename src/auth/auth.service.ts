0e2566dde02a1ee0f9c41bbd182413aba1479751
Caroline Jeldres <cjeldresm@gmail.com>
2024-07-15 10:02:21 -0400

    authorized

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,11 +1,12 @@
-import { BadRequestException, Injectable, InternalServerErrorException } from '@nestjs/common';
+import { BadRequestException, Injectable, InternalServerErrorException, UnauthorizedException } from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
 import { Repository } from 'typeorm';
 
 import * as bcrypt from 'bcrypt';
 
 import { User } from './entities/user.entity';
-import { CreateUserDto } from './dto/create-user.dto';
+import { CreateUserDto, LoginUserDto } from './dto';
+
 
 @Injectable()
 export class AuthService {
@@ -26,13 +27,30 @@ export class AuthService {
       })
 
       await this.userRepository.save(user)
-      delete user.password; 
+      delete user.password;
 
       return user;
 
     } catch (error) {
       this.handleDBErrors(error)
     }
+  }
+
+  async login(loginUserDto: LoginUserDto) {
+    const { password, email } = loginUserDto
+
+    const user = await this.userRepository.findOne({
+      where: { email },
+      select: { email: true, password: true }
+    });
+
+    if (!user)
+      throw new UnauthorizedException('Credentials are not valid (email)');
+
+    if (!bcrypt.compareSync(password, user.password))
+      throw new UnauthorizedException('Credentials are not valid (password)');
+
+    return user
   }
 
   private handleDBErrors(error: any): never {
